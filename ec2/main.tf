2b4a31a694991118c94ce496feff7ea501a4149e
Sonali Varshney <sonali.varshney@hcltech.com>
2025-09-09 15:09:06 +0530

    msg

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,26 +5,25 @@ resource "aws_instance" "web_instance" {
   subnet_id = var.subnet_pub_id[count.index] # element(aws_subnet.pubsubnet[*].id, count.index)
   vpc_security_group_ids = var.ec2_sec_gp #[aws_security_group.web_sg.id]
 
-  connection {  #NOTE
-    type        = "ssh"
-    host        = aws_instance.web_instance[count.index].public_ip
-    user        = "ec2-user"
-    private_key = file("./k8s.pem") # Adjust path to your private key
-  }
- 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo dnf update -y",
-      "sudo dnf install -y docker",
-      "sudo systemctl enable docker",
-      "sudo systemctl start docker",
-      "sudo usermod -aG docker ec2-user",
-      "sg docker -c",
-      "sudo mkdir -p /var/lib/openproject/{pgdata,assets}",
-      "sudo docker run -d --name openproject -p 8080:80 -e OPENPROJECT_HOST_NAME=${self.public_ip}:8080 -e OPENPROJECT_SECRET_KEY_BASE=$(openssl rand -hex 32) -e OPENPROJECT_HTTPS=false -v /var/lib/openproject/pgdata:/var/openproject/pgdata -v /var/lib/openproject/assets:/var/openproject/assets openproject/openproject:16"
-    ]
-  }
-
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo dnf update -y
+              sudo dnf install -y docker
+              sudo systemctl enable docker
+              sudo systemctl start docker
+              sudo usermod -aG docker ec2-user
+              newgrp docker
+              sudo mkdir -p /var/lib/openproject/{pgdata,assets}
+              sudo docker run -d \
+                --name openproject \
+                -p 8080:80 \
+                -e OPENPROJECT_HOST_NAME=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4) \
+                -e OPENPROJECT_SECRET_KEY_BASE=$(openssl rand -hex 32) \
+                -e OPENPROJECT_HTTPS=false \
+                -v /var/lib/openproject/pgdata:/var/openproject/pgdata \
+                -v /var/lib/openproject/assets:/var/openproject/assets \
+                openproject/openproject:16
+              EOF
 
   tags = {
     Name = "nginx-instance-${count.index}"
